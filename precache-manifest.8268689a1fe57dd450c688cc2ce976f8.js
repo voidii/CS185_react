dc435829e1ff209201c0000e607948ab91b8d68e
voidii <ruoqian@umail.ucsb.edu>
2020-04-25 22:54:44 -0700

    Updates

---
+++ precache-manifest.8268689a1fe57dd450c688cc2ce976f8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c787a130b68e1ca064c2deae86f9c8b6",
+    "url": "/CS185_react/build/index.html"
+  },
+  {
+    "revision": "d3644f886d7feb1f6f84",
+    "url": "/CS185_react/build/static/css/main.46486d72.chunk.css"
+  },
+  {
+    "revision": "25555988276692ee214e",
+    "url": "/CS185_react/build/static/js/2.20e5e8f3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CS185_react/build/static/js/2.20e5e8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3644f886d7feb1f6f84",
+    "url": "/CS185_react/build/static/js/main.40d65697.chunk.js"
+  },
+  {
+    "revision": "7fd283f85a7cb39c66e5",
+    "url": "/CS185_react/build/static/js/runtime-main.a253adc9.js"
+  },
+  {
+    "revision": "4e346a0a704a4ca7f2b2867d981c2e53",
+    "url": "/CS185_react/build/static/media/Image.4e346a0a.jpg"
+  },
+  {
+    "revision": "aae0c6ecdd464857b87e22cabbef1b64",
+    "url": "/CS185_react/build/static/media/M1.aae0c6ec.png"
+  },
+  {
+    "revision": "7c17f3542c3770195cce0825246ce344",
+    "url": "/CS185_react/build/static/media/M2.7c17f354.png"
+  },
+  {
+    "revision": "c2c16fd026d271882146900de4d0d46e",
+    "url": "/CS185_react/build/static/media/M3.c2c16fd0.png"
+  },
+  {
+    "revision": "6178340a73e1ff85db5dc0cfab5c30f5",
+    "url": "/CS185_react/build/static/media/M6.6178340a.png"
+  },
+  {
+    "revision": "09e3babe4d9545de56eb4e81ccca6313",
+    "url": "/CS185_react/build/static/media/w1.09e3babe.png"
+  },
+  {
+    "revision": "e68acace672865580a4fc9447e7fcfd3",
+    "url": "/CS185_react/build/static/media/w2.e68acace.png"
+  },
+  {
+    "revision": "e1c776457780c15875f4d26d3d295726",
+    "url": "/CS185_react/build/static/media/w3.e1c77645.png"
+  },
+  {
+    "revision": "ab508d6bf3488a629b69660976e2aa6f",
+    "url": "/CS185_react/build/static/media/w4.ab508d6b.png"
+  },
+  {
+    "revision": "f2b42d60ef904e12faced04a3777cc62",
+    "url": "/CS185_react/build/static/media/w5.f2b42d60.png"
+  },
+  {
+    "revision": "25078022e303fea8ae8d6616ee0c97b5",
+    "url": "/CS185_react/build/static/media/w6.25078022.png"
+  },
+  {
+    "revision": "dac92cf9eddc0c227b0645b3a2bcc5b3",
+    "url": "/CS185_react/build/static/media/w7.dac92cf9.png"
+  },
+  {
+    "revision": "6606455f9a5c6b8d02b1c007ceba6f43",
+    "url": "/CS185_react/build/static/media/w8.6606455f.png"
+  },
+  {
+    "revision": "76bd0e17c280d139505b743f56ff7d59",
+    "url": "/CS185_react/build/static/media/w9.76bd0e17.png"
+  }
+]);
