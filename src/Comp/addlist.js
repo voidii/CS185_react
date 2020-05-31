f80c37a0b65df3add6a12fc97fd731a0e0e94c3f
voidii <ruoqian@umail.ucsb.edu>
2020-05-30 17:56:37 -0700

    fixed a tiny bug

--- src/Comp/addlist.js
+++ src/Comp/addlist.js
@@ -11,22 +11,22 @@ if (!firebase.apps.length) {
     firebase.initializeApp(config)
 } 
 function uniqueArray(array, key){
-  if(array === null){return array;}
-  var result = [array[0]];
-  for(var i = 1; i < array.length; i++){
-    var item = array[i];
-    var repeat = false;
-    for (var j = 0; j < result.length; j++) {
-      if (item[key] === result[j][key]) {
-        repeat = true;
-        break;
-      }
-    }
-    if (!repeat) {
-      result.push(item);
-    }
-  }
-  return result;
+  //if(array === null){return array;}
+  //var result = [array[0]];
+  //for(var i = 1; i < array.length; i++){
+  //  var item = array[i];
+  //  var repeat = false;
+  //  for (var j = 0; j < result.length; j++) {
+  //    if (item[key] === result[j][key]) {
+  //      repeat = true;
+  //      break;
+  //    }
+  //  }
+  //  if (!repeat) {
+  //    result.push(item);
+  //  }
+  //}
+  //return result;
 }
 function unique(arr){
   var newArr = [];
@@ -63,122 +63,12 @@ class AddList extends Component {
     this.cancel = '';
   }
 
-  fetchSearchResult = ( query ) => {
-    const searchUrl = `https://www.omdbapi.com/?apikey=80b44c2c&i=${
-            query
-        }&plot=full` ;
-    if( this.cancel ){
-      this.cancel.cancel();
-    }
 
-    this.cancel = axios.CancelToken.source();
-
-    axios.get( searchUrl, {
-      CancelToken: this.cancel.token
-    } )
-      .then( res => {
-        const resultNotFoundMsg = ! res.data.Title.length
-                                  ? 'there are no more result, plz try a new onw'
-                                  :'';
-        let Title = res.data.Title;
-        let Year = res.data.Year;
-        let Poster = res.data.Poster;
-        let Director = res.data.Director;
-        let IMDBID = query;
-        if(this.state.all_movies === null){
-          this.setState({
-            all_movies: [{Title, Year, Poster, Director, IMDBID, }],
-            loading: false,
-          })
-        }
-
-        if(this.state.movies === null)
-        {
-          this.setState({
-            movies: [{Title, Year, Poster, Director, IMDBID}],
-            all_movies: this.state.all_movies.concat({Title, Year, Poster, Director, IMDBID}),
-            message: resultNotFoundMsg,
-            loading: false,
-          })
-          this.writeALLData();
-          this.getALLData();
-        }
-        else
-        {
-          const depl = false;//用于id判断重复
-          this.state.movies.map((item,index)=>{
-            if(item.IMDBID == IMDBID){
-                depl = true;
-                console.warn( IMDBID )
-            }
-          })
-          if(depl){
-            this.setState({
-              movies: this.state.movie,
-              message: resultNotFoundMsg,
-              loading: false,
-            })
-          }
-          else{
-            this.setState({
-              movies: this.state.movies.concat({Title, Year, Poster, Director, IMDBID}),
-              all_movies: this.state.all_movies.concat({Title, Year, Poster, Director, IMDBID}),
-              message: resultNotFoundMsg,
-              loading: false,
-            })
-            this.writeALLData();
-            this.getALLData();
-          }
-      }
-      })
-      .catch(error => {
-        if(axios.isCancel(error) || error){
-          this.setState({
-            loading: false,
-            message: 'failed to load data',
-          })
-        }
-        console.warn( error )
-      })
-      
-  };
-
-  writeUserData = () => {
-      firebase.database().ref('/' + this.state.list).set(this.state.movies);
-      console.log('DATA SAVED');
-  }
-  writeALLData = () => {
-    uniqueArray(this.state.all_movies, 'Title')
-    console.log(uniqueArray(this.state.all_movies, 'Title'));
-    firebase.database().ref('/ALL').set(uniqueArray(this.state.all_movies, 'Title'));
-    console.log('ALL DATA SAVED');
-  }
   writeListData = () => {
     firebase.database().ref('/list').set(this.state.ex_list);
     console.log('LIST DATA SAVED');
   }
   
-    
-  getUserData = () => {
-      let ref = firebase.database().ref('/' + this.state.list);
-      ref.on('value', snapshot => {
-        const movies = snapshot.val();
-        this.setState({
-          movies: movies,
-        });
-      });
-      console.log('DATA RETRIEVED');
-  }
-  getALLData = () => {
-    let all = firebase.database().ref('/ALL');
-    all.on('value', snapshot => {
-      const all_movies = snapshot.val();
-      this.setState({
-        all_movies: all_movies,
-      });
-    });
-    console.log('DATA RETRIEVED');
-}
   getListData = () => {
     let ref = firebase.database().ref('/list');
     ref.on('value', snapshot => {
@@ -191,41 +81,10 @@ class AddList extends Component {
 }
   
   componentDidMount() {
-      this.getUserData();
       this.getListData();
-      this.getALLData();
-  }
-    
-  componentDidUpdate(prevProps, prevState) {
-
-      if (prevState !== this.state) {
-        if(prevState.list !== this.state.list){
-          this.getUserData();
-          return;
-        }
-        if(prevState.all_movies !== this.state.all_movies){
-          this.writeALLData();
-          //this.writeMovieListData();
-        }
-        if(prevState.movies !== this.state.movies){
-          this.writeUserData();
-          //this.writeALLData();
-          //this.writeMovieListData();
-        }
-      }
   }
 
-  handleOnInputChange = (event) => {
-    const query = event.target.value;
-    this.setState({ query: query, loading: true,}, () => {
-      this.fetchSearchResult( query );
-    });
-  };
 
-  handleOnDropDownChange = (event) => {
-    const query = event.value;
-    this.setState({ list: query });
-  };
 
   addnewlist = (event) =>{
     let new_list = this.refs.new_list.value;
